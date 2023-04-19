86923a2da22eefd395573f2b1baf4420185679b0
Wendl <kennedyw@tmctrans.com>
2023-04-19 13:39:48 -0500

    added the alphabet

--- list.js
+++ list.js
@@ -1,13 +1,12 @@
-
+var beeNoise = new Audio('bee.mp3');
 let beeButton = document.getElementById("showBee");
 let beeBody = document.getElementById("bee");
 
 beeButton.addEventListener("click", showBee)
 
-
 function showBee() 
 {
+    beeNoise.play();
     beeBody.removeAttribute("hidden");
-    console.log("heyyyyy");
     setTimeout(() => { beeBody.setAttribute("hidden", "");}, 4800); 
 }
